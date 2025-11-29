aea3a4d1cfcb09122721d6b35b5a5fe0de14d478
EndoShogo <tkg.gg.shogo@gmail.com>
2025-11-29 15:43:46 +0900

    requirements.txtをついか

--- deepl.py
+++ deepl.py
@@ -1,6 +1,11 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-AUTH_KEY = "REDACTED_DEEPL_KEY"
+# 環境変数の読み込み
+load_dotenv()
+
+AUTH_KEY = os.getenv("DEEPL_API_KEY", "REDACTED_DEEPL_KEY")
 BASE_URL = "https://api-free.deepl.com/v2/translate"
 
 
